08a518565d0ac659aaefc9ba36fe0099577bc0b0
Jiarui LI <ruiruibupt@126.com>
2017-10-30 20:54:18 +0800

    code clean

--- control.ts
+++ control.ts
@@ -8,11 +8,10 @@ pm2.connect(function (err) {
   }
 
   pm2.start({
-    script:             'script.ts',          // Script to be run
-    exec_mode:          'cluster',         // Allows your app to be clustered
-    instances:          1,                 // Optional: Scales your app by 1
-    name:               'test1',                 // Optional: Pm2 name
-    max_memory_restart: '100M'    // Optional: Restarts your app if it reaches 100M
+    script:             'script.ts',    // Script to be run
+    exec_mode:          'fork',         // Allows your app to be clustered
+    name:               'script',       // Optional: Pm2 name
+    max_memory_restart: '100M'          // Optional: Restarts your app if it reaches 100M
   }, function (err, apps) {
     if (apps[0].pm2_env.pm_id !== undefined) {
       console.log(`start pm2 successfully! pm_id: ${apps[0].pm2_env.pm_id}`)
