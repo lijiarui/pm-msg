e1cc1f769bedde15ff025b47f1ee1a73242a7d29
Jiarui LI <ruiruibupt@126.com>
2017-10-31 14:18:57 +0800

    code clean

--- kill.ts
+++ kill.ts
@@ -1,18 +1,14 @@
 /**
- * kill the pm2 process named with `script`
+ * Kill the pm2 process named with `script`
  */
-
+import { pm2List } from './pm2-service'
 var pm2 = require('pm2')
-pm2.connect(function () {
-  pm2.list(function(err, data) {
-    if (err) throw err
-    data.forEach(pm2Process => {
-      if (pm2Process['name'] === 'script') {
-        console.log(`kill pid ${pm2Process['pid']}`)
-        process.kill(pm2Process['pid'], 'SIGTERM')
-      }
-    })
-  })
+pm2.connect(async function () {
+  const list = await pm2List()
+  list.forEach(pm2Process => (pm2Process.name === 'script') && process.kill(pm2Process.pid, 'SIGTERM') )
+  pm2.disconnect()
 })
+
+// Should add detect whether kill successfully function
 
 
--- say.ts
+++ say.ts
@@ -1,3 +1,6 @@
+import { pm2List } from './pm2-service'
+const pm2Name = 'script'
+const ONLINE = 'online'
 var pm2 = require('pm2')
 
 let option = {
@@ -9,29 +12,27 @@ let option = {
 }
 
 // send message by pm2 name
-// only send message once to cluster
-pm2.connect(function () {
-  pm2.list(function(err, data) {
-    if (err) throw err
-    for (let i in data) {
-      let pm2Process = data[i]
-      if (pm2Process['name'] === 'script' && pm2Process['pm2_env']['status'] === 'online') {
-        option['id'] = pm2Process['pm_id']
-        pm2.sendDataToProcessId(option['id'], option, function (err, res) {
-          if (err) throw err
-          console.log(option)
-          console.log('send the following message successfully!')
-        })
-        return
-      }
+pm2.connect(async function () {
+  const list = await pm2List()
+  // only send message once to cluster
+  for (let i in list) {
+    let pm2Process = list[i]
+    if (pm2Process.name === pm2Name && pm2Process.pm2_env.status === ONLINE) {
+      option['id'] = pm2Process.pm_id
+      pm2.sendDataToProcessId(option['id'], option, function (err, res) {
+        if (err) throw err
+        console.log(option)
+        console.log('send the following message successfully!')
+      })
+      return
     }
-  })
-  
-  pm2.launchBus(function (err, pm2_bus) {
-    pm2_bus.on('bot:msg', function (packet) {
-      console.log('get response:')
-      console.log(packet)
-      pm2.disconnect()
-    })
+  }
+})
+
+pm2.launchBus(function (err, pm2_bus) {
+  pm2_bus.on('bot:msg', function (packet) {
+    console.log('get response:')
+    console.log(packet)
+    pm2.disconnect()
   })
 })
